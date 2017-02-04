e3f94e04af1dea103ba14fac3059bb05da1f8c16
vaibhav-k <vaibhav1kulshrestha@gmail.com>
2017-02-04 19:09:37 +0530

    Learn Python The Hard Way questions

---
+++ LPTHW/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMarch\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months", months
+
+print """
+There'ssomething going on here.
+With three double quotes.
+We'll beable to type sa much we like
+Even 4 lines ifwe want, or 5, or 6
+"""
